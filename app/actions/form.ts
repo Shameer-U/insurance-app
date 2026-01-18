fa568779cd7562e261c34bc3f7e9d12f7a975cf9
Shameer-U <shameerushahul@gmail.com>
2026-01-18 22:37:22 +0530

    create addForm server action

--- app/actions/form.ts
+++ app/actions/form.ts
@@ -1,10 +1,26 @@
 "use server";
 
-import { readForms } from "@/data/jsonDB";
+import { readForms, writeForm } from "@/data/jsonDB";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 export const getForms = async () => {
   try {
     return readForms();
+  } catch (error) {
+    throw new Error("Something went wrong", { cause: error });
+  }
+};
+
+export const addForm = async (formData: any) => {
+  //checking for session since it is exposed  to client
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+
+  try {
+    return writeForm(formData);
   } catch (error) {
     throw new Error("Something went wrong", { cause: error });
   }
